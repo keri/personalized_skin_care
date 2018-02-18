2b1e3f6453eaf642048a8bc2a3c03b0d81572cf1
Keri Multerer <keri@multerer.com>
2018-02-17 23:11:35 -0800

    modified tfidf pipeline for new products and worked on changing format of website and more flexibility for concerns

--- website/model.py
+++ website/model.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import psycopg2
 import os
-#from queries import PostGres
 
 password = os.environ['DB_PASS']
 user = os.environ['DB_USER_NAME']
@@ -17,18 +16,6 @@ class DataModel(object):
                                     password=password,user=user)
         self.product_list = []
         self.products = ['moisturizer','serum']
-
-    # def find_product(self):
-    #     '''Write some logic to determine what product should be used based on the budget they have left
-    #     and what products they have in their basket'''
-    #     if len(self.basket) == 0:
-    #         self.product = 'moisturizer'
-    #     if 'moisturizer' in self.basket:
-    #         self.product = 'serum'
-    #     elif 'serum' in self.basket:
-    #         self.product = 'cleanser'
-    #     else:
-    #         self.product = 'end'
 
     def skin_concerns(self,concern_list):
         n = len(concern_list)
@@ -49,9 +36,18 @@ class DataModel(object):
     SELECT
       DISTINCT({table}.asin)
     , productinfo.title
-    , {table}.{self.concern1}
-    , {table}.{self.concern2}
-    , {table}.{self.concern3}
+    , {table}.scent
+    , {table}.dry
+    , {table}.oily
+    , {table}.combination
+    , {table}.antiaging
+    , {table}.psoriasis
+    , {table}.lightening
+    , {table}.sensitive
+    , {table}.scent
+    , {table}.dry
+
+    , {table}.oily
     , productinfo.price
     , productinfo.numberreviews
     , productinfo.imageurl
@@ -77,17 +73,12 @@ class DataModel(object):
         self.budget += self.budget*.1
         cur = self.conn.cursor()
         self.concern1,self.concern2,self.concern3 = self.skin_concerns(concern_list)
-#        count = 1
         for product in self.products:
             self.product = product
-#            query = self.query(concern1,concern2,concern3,budget)
             query = self.query()
             self.run_query(query)
-#            temp_budget = self.cleaned_df['new_budget']
-#            product_count = 1
-#            while self.cleaned_df['new_budget'].max() > 0 and self.product != 'end':
 
-            for concern in concern_list:
+            for concern in [self.concern1,self.concern2,self.concern3]:
                 product_df = self.get_products(concern)
                 product_df.rename(columns = {'p_'+self.concern1 : 'concern1',
                                              'p_'+self.concern2 : 'concern2',
@@ -95,33 +86,12 @@ class DataModel(object):
 
 
                 self.product_list.extend(product_df.to_dict(orient='records'))
-#                 for idx in product_df.index:
-#                     count = int(count)
-#                     count += 1
-#                     count = str(count)
-# #                    idx_list = product_df.index
-#                     self.product_dictionary['product'+ count] =  product_df.loc[int(idx)].to_dict()
-        print(self.product_list)
+
         return(self.product_list)
 
     def get_product_list_(self):
         return(self.product_list)
-                    # self.baskets['product'+str(count+1)] =  product_df.loc[1]}
-                    # self.baskets['basket'+str(count+2)] =  product_df.loc[2]}
-#                    count += 3
-#            product_count += 1
-            # self.product = self.find_product()
-            # budget = self.cleaned_df['new_budget'].max()
-            # query = self.query(concern1,concern2,concern3,budget)
-            # temp = self.cleaned_df['new_budget']
-            # self.cleaned_df = self.clean_df(df_results,concern1,concern2,concern3,budget)
-            # self.cleaned_df['new_budget'] = temp
 
-#             self.product = self.find_product()
-#                 self.basket = self.find_top_products(combined_df)
-#
-#             return(self.basket, combined_df)
-# #            cur.rollback()
 
     def update_price(self):
         mask = self.cleaned_df.price <= self.cleaned_df.new_budget
@@ -136,38 +106,51 @@ class DataModel(object):
         df['review_ratio'] = df.numberreviews / 350
         df['new_budget'] = self.budget - df['price']
         df[[self.concern1,self.concern2,self.concern3]] = df[[self.concern1,self.concern2,self.concern3]].apply(pd.to_numeric)
-#        df['review_ratio'] = df['weighted_reviews'] / 350
+        #normalizes the concerns to the number of reviews the product recieved.
         df['weighted_'+ self.concern1]  = df['review_ratio'] * df[self.concern1]
         df['weighted_' + self.concern2] = df['review_ratio'] * df[self.concern2]
         df['weighted_'+ self.concern3] = df['review_ratio'] * df[self.concern3]
         df['total'] = (df['weighted_'+ self.concern1] + df['weighted_'+ self.concern2] + df['weighted_'+ self.concern3])
-        df['p_'+self.concern1] = df['weighted_'+ self.concern1] / (df.total+.000001)
-        df['p_'+self.concern2] = df['weighted_'+ self.concern2] / (df.total+.000001)
-        df['p_'+self.concern3] = df['weighted_'+ self.concern3] / (df.total+.000001)
-        df = df.round(2)
+        df['p_'+self.concern1] = df['weighted_'+ self.concern1] / df.total
+        df['p_'+self.concern2] = df['weighted_'+ self.concern2] / df.total
+        df['p_'+self.concern3] = df['weighted_'+ self.concern3] / df.total
+        #taking the unweighted totals as well to randomly include products with high scores but less reviews.
+        df['low_reviews'] = 0
+        mask = df['review_ratio'] < .06
+        column_name = 'low_reviews'
+        df.loc[mask, column_name] = 1
+        df['unweighted_'+ self.concern1]  = df['low_reviews'] * df[self.concern1]
+        df['unweighted_' + self.concern2] = df['low_reviews'] * df[self.concern2]
+        df['unweighted_'+ self.concern3] = df['low_reviews'] * df[self.concern3]
+        df['unweighted_total'] = (df['unweighted_'+ self.concern1] + df['unweighted_'+ self.concern2] + df['unweighted_'+ self.concern3])
+        df['p_unweighted_'+self.concern1] = df['unweighted_'+ self.concern1] / df.unweighted_total
+        df['p_unweighted_'+self.concern2] = df['unweighted_'+ self.concern2] / df.unweighted_total
+        df['p_unweighted_'+self.concern3] = df['unweighted_'+ self.concern3] / df.unweighted_total
+        df.fillna(0,inplace=True)
         return(df)
 
     def get_products(self, concern):
-        self.cleaned_df.sort_values(by=[concern],ascending=False,inplace=True)
-        three_products_for_concern = self.cleaned_df.head(3)
-        three_products_for_concern_idx = three_products_for_concern.index
-        self.cleaned_df = self.cleaned_df.drop(three_products_for_concern_idx, axis=0)
-        # mask = self.cleaned_df.price <= self.cleaned_df.new_budget
-        # column_name = 'new_budget'
-        # self.cleaned_df.loc[mask, column_name] = self.cleaned_df.new_budget - self.cleaned_df.price
+        #grab top two product with weighted review ratio
+        temp = pd.DataFrame()
+        for i in range(2):
+            top_one = self.cleaned_df.sort_values(by=['weighted_'+concern],ascending=False)
+            top_one = top_one.head(1)
+            top_one_idx = top_one.index[0]
+            if top_one.loc[top_one_idx,'p_'+concern] == 0:
+                top_one_low_review = self.cleaned_df.sort_values(by=['unweighted_'+concern],ascending=False)
+                top_one_low_review['p_' + concern] = top_one_low_review['p_unweighted_'+ concern]
+                top_one = top_one_low_review
+            top_one_idx = top_one.index
+            self.cleaned_df = self.cleaned_df.drop(top_one_idx,axis=0)
+            temp = pd.concat([top_one,temp])
+
+        #grab top product with low reviews but high score for concern
+        top_one_low_review = self.cleaned_df.sort_values(by=['unweighted_'+concern],ascending=False)
+        top_one_low_review['p_' + concern] = top_one_low_review['p_unweighted_'+ concern]
+        top_one_low_review_index = top_one_low_review.head(1).index
+        self.cleaned_df = self.cleaned_df.drop(top_one_low_review_index,axis=0)
+        three_products_for_concern = pd.concat([temp,top_one_low_review.head(1)])
         three_products_for_concern = three_products_for_concern.filter(
                                     ['asin','price','title','p_'+self.concern1,'p_'+self.concern2,
                                     'p_'+self.concern3,'new_budget','imageurl'],axis=1)
         return(three_products_for_concern)
-
-
-#     def find_top_products(self,combined_df):
-#         '''get box of several boxes of products. Include logic that
-#         takes budget and percent of aoc covered for each product'''
-# #        moisturizer =
-#
-#         self.basket['product1'] = 'moisturizerA'
-#         self.basket['product2'] = 'serumB'
-#         self.basket['product3'] = 'cleanserC'
-#         self.basket['price'] = 23.00
-#         return(self.basket)
