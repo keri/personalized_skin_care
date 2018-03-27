3d94423e7df8d1e7f83e52310d5d48c08ddfb99d
Keri Multerer <keri@multerer.com>
2018-03-27 15:36:57 -0700

    created a new upload image template, added /uploader path to app.py, and changed starter_skin deleting file upload button

--- website/app.py
+++ website/app.py
@@ -1,37 +1,30 @@
-from flask import Flask, render_template, request, jsonify, redirect, abort
+from flask import Flask, render_template, request, jsonify, redirect, abort, url_for
+from werkzeug import secure_filename
 from model import DataModel
 #from baskets import Baskets
 import json
+import os
+
+UPLOAD_FOLDER = 'data/'
+ALLOWED_EXTENSIONS = set(['txt','pdf', 'png', 'jpg', 'jpeg', 'gif','docx','doc'])
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 data_model = DataModel()
 #baskets = Baskets()
+def allowed_file(filename):
+    print('filename = ',filename)
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def index():
     print('in index')
-    return render_template('starter_skin.html')
+    return render_template('start.html')
 
 
 @app.route("/results")
-def results():
-    c1 = request.args.get('c1')
-    c2 = request.args.get('c2')
-    c3 = request.args.get('c3')
-    budget = request.args.get('budget')
-    concern_list = [c1,c2,c3]
-    products = data_model.get_recommendations(budget, concern_list)
-    results = {
-        'c1':c1.title(),
-        'c2':c2.title(),
-        'c3':c3.title(),
-        'budget':budget,
-        'products':products
-    }
-    return render_template('results.html', results=results)
-
-
-@app.route("/results2")
 def results2():
     concerns = []
     concern_list = ['dry','oily','combination','skintone','eyes','antiaging','psoriasis','lightening','sunscreen',
@@ -49,6 +42,37 @@ def results2():
         'products':products
     }
     return render_template('results.html', results=results)
+
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print('file = ',filename)
+            print('saved in ',app.config['UPLOAD_FOLDER'])
+            return render_template('starter_skin.html')
+        return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+          <p><input type=file name=file>
+             <input type=submit value=Upload>
+        </form>
+        '''
+
+
 
 @app.route("/results3")
 def results3():
