618720f419e50a3a5ca9003c451a8d575e08d22f
Keri Multerer <keri@multerer.com>
2018-03-28 13:01:56 -0700

    added functionality to upload 3 imgaes

--- website/app.py
+++ website/app.py
@@ -1,37 +1,66 @@
-from flask import Flask, render_template, request, jsonify, redirect, abort, url_for
+from flask import Flask, render_template, request, jsonify, redirect, abort, url_for, flash, session
 from werkzeug import secure_filename
 from model import DataModel
+from os import listdir
+import csv
+
 #from baskets import Baskets
 import json
 import os
 
-UPLOAD_FOLDER = 'data/'
+
+UPLOAD_FOLDER = 'data/images'
 ALLOWED_EXTENSIONS = set(['txt','pdf', 'png', 'jpg', 'jpeg', 'gif','docx','doc'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "super secret key"
 
 data_model = DataModel()
 #baskets = Baskets()
 def allowed_file(filename):
-    print('filename = ',filename)
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def update_csv(image_list):
+    print(image_list)
+    training_path = ('/Users/keri/git/galvanize/capstone/psc/website/data/training/images_for_training.csv')
+    '''updates the csv with new images'''
+    with open(training_path,"a",newline='') as f:  
+        cw = csv.writer(f)
+        for row in image_list:
+            cw.writerow([row])
 
-@app.route('/')
-def index():
-    print('in index')
-    return render_template('start.html')
 
-
-@app.route("/results")
-def results2():
+def get_concerns():
     concerns = []
     concern_list = ['dry','oily','combination','skintone','eyes','antiaging','psoriasis','lightening','sunscreen',
                 'night','pores','sensitive']
     for concern in concern_list:
         if request.args.get(concern):
             concerns.append(concern)
+    return(concerns)
+
+def upload_images(concern_list):
+    '''image was saved from start.html into the data/folder. Put an exention on the filename
+    for each concern so the model can see what images are attached to which concern.'''
+    converted_files = []
+    files = listdir('/Users/keri/git/galvanize/capstone/psc/website/data/images')
+    for file in files:
+        for concern in concern_list:
+            converted_files.append(concern + '+' + file)
+        os.remove('/Users/keri/git/galvanize/capstone/psc/website/data/images/'+file)
+    update_csv(converted_files)
+
+
+@app.route('/')
+def index():
+    return render_template('start.html')
+
+
+@app.route("/results")
+def results2():
+    concerns = get_concerns()
+    upload_images(concerns)
  #   concerns = list(filter((lambda x: request.args.get(x)), concern_list))
     print(concerns)
     budget = request.args.get('budget')
@@ -46,31 +75,36 @@ def results2():
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print('file = ',filename)
-            print('saved in ',app.config['UPLOAD_FOLDER'])
-            return render_template('starter_skin.html')
-        return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-          <p><input type=file name=file>
-             <input type=submit value=Upload>
-        </form>
-        '''
+
+    if request.method == 'POST' and 'file' in request.files:
+        for f in request.files.getlist('file'):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print( 'Upload completed.' )
+    return render_template('starter_skin.html')
+
+
+
+
+
+    #     # if user does not select file, browser also
+    #     # submit a empty part without filename
+    #     if file.filename == '':
+    #         flash('No selected file')
+    #         return redirect(request.url)
+    #     if file1 and allowed_file(file1.filename):
+    #         filename = secure_filename(file.filename)
+    #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #         return render_template('starter_skin.html')
+    #     return '''
+    #     <!doctype html>
+    #     <title>Upload new File</title>
+    #     <h1>Upload new File</h1>
+    #     <form method=post enctype=multipart/form-data>
+    #       <p><input type=file name=file>
+    #          <input type=submit value=Upload>
+    #     </form>
+    #     '''
 
 
 
@@ -94,4 +128,7 @@ def results3():
 
 
 if __name__ == '__main__':
+
+#    app.debug = True
     app.run(host='0.0.0.0', threaded=True)
+
