448aa2262897af1db46d1709b96074f5296604e1
Keri Multerer <keri@multerer.com>
2018-04-18 20:20:34 -0700

    reformatted website and simplified into several pages.

--- website/app.py
+++ website/app.py
@@ -11,8 +11,6 @@ from boto.s3.key import Key
 import itertools
 import random
 import ast
-
-#from baskets import Baskets
 import json
 import os
 
@@ -68,6 +66,7 @@ def upload_images_to_s3(filename, path):
 def create_baskets(products, concerns):
     '''creating a basket of products in which product totals for each concern is greater than .9'''
     moisturizers = [result for result in products if result['category'] == 'moisturizer']
+    print('moisturizers = ',moisturizers)
     serums = [result for result in products if result['category'] == 'serum']
     cleansers = [result for result in products if result['category'] == 'cleanser']
 
@@ -78,7 +77,10 @@ def create_baskets(products, concerns):
         and appends to final list that comtains each basket as an element'''
     combo_idx = 0
     basket_count = 0
+
     for combination in combinations:
+        basket_price = (combination[0]['price'] + combination[1]['price'] + 
+                        combination[2]['price'])
         basket_concerns={}
         for concern in concerns:
             concern_total = (combination[0][concern] + combination[1][concern] + 
@@ -88,7 +90,14 @@ def create_baskets(products, concerns):
             temp_basket = {}
             temp_basket['products'] = combinations[combo_idx]
             temp_basket['basket_concerns'] = basket_concerns
-
+            temp_basket['price'] = round(basket_price,2)
+            basket_dictionary[basket_count] = temp_basket
+            basket_count += 1
+        elif all(i >= .5 for i in basket_concerns.values()):
+            temp_basket = {}
+            temp_basket['products'] = combinations[combo_idx]
+            temp_basket['basket_concerns'] = basket_concerns
+            temp_basket['price'] = round(basket_price,2)
             basket_dictionary[basket_count] = temp_basket
             basket_count += 1
         combo_idx += 1
@@ -96,7 +105,11 @@ def create_baskets(products, concerns):
 
 @app.route('/')
 def index():
-    return render_template('spa.html')
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/uploader', methods = ['GET','POST'])
 def upload_file():
@@ -106,15 +119,15 @@ def upload_file():
             filename = secure_filename(file.filename)
             print('filename')
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return render_template('spa.html')
+    return render_template('questionnaire.html')
 
-@app.route('/questions', methods = ['GET','POST'])
+@app.route('/questions', methods=['GET','POST'])
 def inputquestions():
     day_routine = request.form.getlist('day-routine')
     night_routine = request.form.getlist('night-routine')
     age = request.form.get('age')
     skintone = request.form.get('ethnicity-img')
-    return (render_template('spa.html'))
+    return (render_template('areas_of_concern.html'))
 
 @app.route("/results", methods=['GET','POST'])
 def results2():
@@ -154,6 +167,33 @@ def cleanser_results():
     concerns = request.form.get('concerns')
     return render_template('cleansers.html', cleansers=ast.literal_eval(cleansers), 
                             concerns=ast.literal_eval(concerns))
+
+# @app.route("/new_basket", methods=["POST"])
+# def new_basket():
+#     new_baskets = request.form.get('baskets')
+#     return render_template('spa_results.html', baskets=ast.literal_eval(new_baskets))
+
+@app.route("/subscribe", methods=['GET'])
+def subscribe_get():
+    return render_template('subscribe.html')
+
+@app.route("/subscribe", methods=['POST'])
+def subscribe_post():
+    email = request.form.get('email')
+    print(email)
+    return redirect('/')
+
+@app.route("/upload_images", methods=["GET","POST"])
+def upload():
+    return render_template('upload_images.html')
+
+@app.route("/questionnaire",methods=["POST"])
+def questionnaire():
+    return render_template('questionnaire.html')
+
+@app.route("/contact",methods=["GET"])
+def contact_get():
+    return render_template('contact.html')
 
 
 
