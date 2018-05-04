f27437ac76a0bb959373e7e19fd42c3601ec5da3
Keri Multerer <keri@multerer.com>
2018-05-03 19:57:50 -0700

    cleaned up templates, worked on navigation bar, and color scheme

--- website/app.py
+++ website/app.py
@@ -63,6 +63,18 @@ def upload_images_to_s3(filename, path):
     k.key = 'test/'+filename
     k.set_contents_from_filename(path_file)
 
+def get_basket_price(basket):
+    return(round((basket[0]['price'] + basket[1]['price'] + basket[2]['price']),2))
+
+def get_basket_concerns(basket, concerns):
+    basket_concerns = {}
+    for concern in concerns:
+        concern_total = (basket[0][concern] + basket[1][concern] + 
+                        basket[2][concern])
+        basket_concerns[concern] = concern_total
+    return(basket_concerns)       
+
+
 def create_baskets(products, concerns):
     '''creating a basket of products in which product totals for each concern is greater than .9'''
     moisturizers = [result for result in products if result['category'] == 'moisturizer']
@@ -78,13 +90,9 @@ def create_baskets(products, concerns):
     basket_count = 0
 
     for combination in combinations:
-        basket_price = (combination[0]['price'] + combination[1]['price'] + 
-                        combination[2]['price'])
-        basket_concerns={}
-        for concern in concerns:
-            concern_total = (combination[0][concern] + combination[1][concern] + 
-                            combination[2][concern])
-            basket_concerns[concern] = concern_total
+        basket_price = get_basket_price(combination)
+        basket_concerns = get_basket_concerns(combination, concerns)
+
         if all(i >= .9 for i in basket_concerns.values()):
             temp_basket = {}
             temp_basket['products'] = combinations[combo_idx]
@@ -99,21 +107,57 @@ def create_baskets(products, concerns):
             temp_basket['price'] = round(basket_price,2)
             basket_dictionary[basket_count] = temp_basket
             basket_count += 1
+        elif all(i >= .5 for i in basket_concerns.values()):
+            temp_basket = {}
+            temp_basket['products'] = combinations[combo_idx]
+            temp_basket['basket_concerns'] = basket_concerns
+            temp_basket['price'] = round(basket_price,2)
+            basket_dictionary[basket_count] = temp_basket
+            basket_count += 1
+        else:
+            temp_basket = {}
+            temp_basket['products'] = combinations[combo_idx]
+            temp_basket['basket_concerns'] = basket_concerns
+            temp_basket['price'] = round(basket_price,2)
+            basket_dictionary[basket_count] = temp_basket
+            basket_count += 1
         combo_idx += 1
     return(basket_dictionary, moisturizers, serums, cleansers)
+
+
+def replace_product(basket, product_list, category, concerns):
+    new_basket = {}
+    products = basket['products']
+
+    print('products = ', products)
+    print('category = ',category)
+    for product in products:
+        if product['category'] == category:
+            lbasket = list(products)
+            product_idx = lbasket.index(product)
+            lbasket.remove(product)
+            new_product = random.choice(product_list)
+            lbasket.insert( product_idx, new_product)
+            new_basket['price'] = get_basket_price(lbasket)
+            new_basket['basket_concerns'] = get_basket_concerns(lbasket, concerns)
+            new_basket['products'] = tuple(lbasket)
+
+            return(new_basket)
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/about')
+@app.route('/faq')
 def about():
-    return render_template('about.html')
+    return render_template('faq.html')
 
-@app.route('/uploader', methods = ['GET','POST'])
+@app.route('/uploader', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        for f in request.files:
+    '''request.files returns an immutable dictionary with {label name, <FileStorage: 'filename'}'''
+    for f in request.files:
+        if request.files[f]:
             file = request.files[f]
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -121,10 +165,15 @@ def upload_file():
 
 @app.route('/questions', methods=['GET','POST'])
 def inputquestions():
-    day_routine = request.form.getlist('day-routine')
-    night_routine = request.form.getlist('night-routine')
+    day_routine = request.form.get('day-routine')
+    night_routine = request.form.get('night-routine')
     age = request.form.get('age')
     skintone = request.form.get('ethnicity-img')
+    gender = request.form.get('gender')
+    print('day routine = ',day_routine)
+    print('age = ',age)
+    print('skine tone = ',skintone)
+    print('gender = ',gender)
     return (render_template('areas_of_concern.html'))
 
 @app.route("/results", methods=['GET','POST'])
@@ -135,6 +184,7 @@ def results2():
     budget = request.form.get('budget')
     products = data_model.get_recommendations(budget, concerns)
     baskets, moisturizers, serums, cleansers = create_baskets(products, concerns)
+    basket = baskets.popitem()[1]
 
     results = {
         'concerns':concerns,
@@ -142,13 +192,9 @@ def results2():
         'products':products
     }
 
-    jresults = json.dumps({'one':1, 'two':2})
-    print(type(jresults))
-    print(type(moisturizers))
-
     return render_template('spa_results.html', results=results, baskets=baskets, 
                             moisturizers=moisturizers,serums=serums,cleansers=cleansers,
-                            jresults=jresults)
+                            basket=basket)
 
 @app.route("/moisturizers", methods=['POST'])
 def moisturizer_results():
@@ -188,10 +234,38 @@ def upload():
 def questionnaire():
     return render_template('questionnaire.html')
 
+@app.route("/areas_of_concern",methods=["GET"])
+def input_concerns():
+    return render_template('areas_of_concern.html')
+
 @app.route("/contact",methods=["GET"])
 def contact_get():
     return render_template('contact.html')
 
+
+@app.route("/new_product",methods=["POST"])
+def replace_product_in_basket():
+    product_list = []
+    basket = ast.literal_eval(request.form.get('basket'))
+    moisturizers = ast.literal_eval(request.form.get('moisturizers'))
+    serums = ast.literal_eval(request.form.get('serums'))
+    cleansers = ast.literal_eval(request.form.get('cleansers'))
+    baskets = ast.literal_eval(request.form.get('baskets'))
+    results = ast.literal_eval(request.form.get('results'))
+    concerns = ast.literal_eval(request.form.get('concerns'))
+    category = request.form.get('category')
+    if category == 'moisturizer':
+        product_list = moisturizers
+    elif category == 'cleanser':
+        product_list = cleansers
+    else:
+        product_list = serums
+
+    new_basket = replace_product(basket, product_list, category, concerns)
+
+    return render_template('spa_results.html', results=results, baskets=baskets, 
+                            moisturizers=moisturizers,serums=serums,cleansers=cleansers,
+                            basket=new_basket)
 
 
 
--- website/static/js/results.js
+++ website/static/js/results.js
@@ -1,30 +1,27 @@
 alert("Hi Keri - root js");
 
-//$("#basket_price").value = "monkey breath"
- // import Products from "./products"
-
 function testFunction( someString ) {
-	var product = someString.shift()
-	someString.unshift(product)
-	var price =  product['price'] ;
-	alert( product.price );
-	document.write(product)
+	var product = someString.shift();
+	someString.push(product);
+	// var price =  product['price'] ;
+	// alert( product.price );
+	// // document.write(product)
 	return(product);
 
-}
+};
 
 
 
 function init_clicks(){
 	var mbtn = $('#btn-moisturizer')[0];
 	mbtn.onclick = function(){
-		alert("was clicked");
+		product = testFunction(jmoisturizers)
+		var title = document.getElementById("title")
+		title.innerHTML = product.title
+		var concern = $('#concern')[0].innerText
+		alert(concern)
+
 	};
 
-}
+};
 
-// function getProduct( productList ) {
-// 	$('#basket_concerns')[0].innerText = testFunction(productList)
-
-
-// }
