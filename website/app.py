6ccbab480ff266ecec44ed3d78b16ffe1fc63672
Keri Multerer <keri@multerer.com>
2018-06-19 13:17:48 -0700

    made a questionnaire_temp and combined the questions with areas of concern

--- website/app.py
+++ website/app.py
@@ -154,7 +154,7 @@ def replace_product(basket, product_list, category, concerns):
 
 @app.route('/')
 def index():
-    return render_template('questionnaire.html')
+    return render_template('questionnaire_temp.html')
 
 @app.route('/faq')
 def about():
